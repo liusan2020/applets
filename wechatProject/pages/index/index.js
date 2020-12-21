e5e26cc5ea72d7db2ab1aa33b6cd1289184e88e1
liusan2020 <378876451@qq.com>
2020-12-22 00:34:53 +0800

    微信小程序

--- wechatProject/pages/index/index.js
+++ wechatProject/pages/index/index.js
@@ -1,5 +1,6 @@
 //引入promise 封装的请求函数
 import { request } from "../../request/index.js";
+const ImgLoader = require('../../img-loader/img-loader.js');
 //index.js
 //获取应用实例
 const app = getApp()
@@ -11,51 +12,56 @@ Page({
     hotImg: null,
     hotList: [],
     recommendImg: null,
-    recommendList: []
+    recommendList: [],
   },
 
+  onLoad: function () {
+    this.imgLoader = new ImgLoader(this);
+  },
   onShow: function () {
+
+  },
+  imgLoaded: function (e) {
+
+  },
+  /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+  onReady: function () {
     this.getIndexInfo();
   },
   async getIndexInfo() {
-    const indexData = wx.getStorageSync('indexData') || {};
-    if (indexData.time && (Date.now() - indexData.time > 1000 * 1800)) {
-      //缓存中有数据且没有过期直接渲染 过期时间为半小时
-      this.setData({
-        swipList: indexData.swiper,
-        navList: indexData.navs,
-        hotImg: indexData.hotImg,
-        hotList: indexData.hotList,
-        recommendImg: indexData.recommendImg,
-        recommendList: indexData.recommendList
-      });
-    } else {
-      //缓存中没有数据则请求数据并放到缓存中
-      //请求轮播图数据
-      const swiper = await request({ url: 'swiper' });
-      //请求导航栏数据
-      const navs = await request({ url: 'navs' });
-      //请求火爆热卖数据
-      const hot = await request({ url: 'hot' });
-      //请求推荐数据
-      const tj = await request({ url: 'tj' });
-      indexData.swiper = swiper.data.message;
-      indexData.navs = navs.data.message;
-      indexData.hotImg = hot.data.hot_img;
-      indexData.hotList = hot.data.message;
-      indexData.recommendImg = tj.data.tj_img;
-      indexData.recommendList = tj.data.message;
-      //设置数据
-      this.setData({
-        swipList: indexData.swiper,
-        navList: indexData.navs,
-        hotImg: indexData.hotImg,
-        hotList: indexData.hotList,
-        recommendImg: indexData.recommendImg,
-        recommendList: indexData.recommendList
-      });
+    let indexData = wx.getStorageSync('indexData') || {};
+    if (!(indexData.time && (Date.now() - indexData.time > 1000 * 60))) {
+      const res = await request({ url: 'page' });
+      indexData = res.data.message;
       indexData.time = Date.now();
       wx.setStorageSync('indexData', indexData);
     }
+
+    this.loadImgs(indexData.swiper);
+    this.loadImgs(indexData.navs);
+    this.loadImgs(indexData.hot_data);
+
+    this.setData({
+      swipList: indexData.swiper,
+      navList: indexData.navs,
+      hotImg: indexData.hot_img,
+      hotList: indexData.hot_data,
+      recommendImg: indexData.tj_img,
+      recommendList: indexData.tj_data
+    });
   },
+  loadImgs(loadObj) {
+    loadObj.forEach(item => {
+      item.loaded = 'false';
+      this.imgLoader.load(item.image_src, (err, data) => {
+        //console.log('图片加载完成', err, data.src, data.width, data.height)
+        if (item.image_src == data.src) {
+          item.loaded = 'true';
+        }
+      })
+    });
+  }
 })
+
--- wechatProject/request/index.js
+++ wechatProject/request/index.js
@@ -1,6 +1,6 @@
 //引用方式 ：import { } from "这个方法的文件所在路径";
 export const request = (params) => {
-    const baseUrl = 'http://liusan.natapp1.cc/wx/api/datas/';
+    const baseUrl = 'http://liusan.natapp1.cc/think/public/wx/api/datas/';
     return new Promise((resolve, reject) => {
         wx.request({
             ...params,
